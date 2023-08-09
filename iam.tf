e5985e8c91fd215c1386e4e2b3fe2bcc8eb67581
Tony Bower <tony.bower@madelabs.io>
2023-08-09 12:08:32 -0400

    Add extra IAM permissions for role

--- iam.tf
+++ iam.tf
@@ -72,3 +72,15 @@ resource "aws_iam_role" "codebuild_role" {
   assume_role_policy   = data.aws_iam_policy_document.codebuild_assume_role_policy_document.json
   permissions_boundary = var.permissions_boundary == "" ? null : var.permissions_boundary
 }
+
+resource "aws_iam_policy" "codebuild_extra_policy" {
+  count  = var.enable_extra_iam_permissions ? 1 : 0
+  name   = "${var.codebuild_project_name}-extra-codebuild-policy"
+  policy = file("./${var.extra_iam_permissions_json_file}")
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild_extra_attachment" {
+  count      = var.enable_extra_iam_permissions ? 1 : 0
+  role       = aws_iam_role.codebuild_role.name
+  policy_arn = aws_iam_policy.codebuild_extra_policy[0].arn
+}
